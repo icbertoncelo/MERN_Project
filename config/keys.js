d28808b6d83d02b11afa1766c31c925c6ed11bd1
Ian Carlos <carlos.ian007@gmail.com>
2019-02-11 17:59:44 -0200

    Adding encryption, jwt and authentication

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://Ian_Carlos:iancarlos1234@ds227255.mlab.com:27255/devconnector_db"
+    "mongodb://Ian_Carlos:iancarlos1234@ds227255.mlab.com:27255/devconnector_db",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
+// Routes
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
-const db = require("./config/keys").mongoURI; //db config
 
-//Connect to MongoDB
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("Hello World"));
 
-//Use routes
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
