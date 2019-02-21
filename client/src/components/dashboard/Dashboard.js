2c0276655b8b87b9da028283e7e2d44bf8e0b0e2
Ian Carlos <carlos.ian007@gmail.com>
2019-02-21 15:04:30 -0300

    Adding Spinner to the Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profileActions";
+
+import Spinner from "../common/Spinner";
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -15,16 +18,30 @@ class Dashboard extends Component {
     let dashboardContent;
 
     if (profile === null || loading) {
-      dashboardContent = <h4>Loading...</h4>;
+      dashboardContent = <Spinner />;
     } else {
-      dashboardContent = <h1>Hello</h1>;
+      // Check if logged in user has profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>DISPLAY PROFILE</h4>;
+      } else {
+        // User is logged in but has no profile
+        dashboardContent = (
+          <div>
+            <p className="lead text muted">Wellcome {user.name}</p>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        );
+      }
     }
 
     return (
       <div className="dashboard">
         <div className="container">
           <div className="row">
-            <div className="col-md12">
+            <div className="col-md-12">
               <h1 className="display-4">Dashboard</h1>
               {dashboardContent}
             </div>
